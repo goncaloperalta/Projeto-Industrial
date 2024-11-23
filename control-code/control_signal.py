9b197fbd7ee703e0649a2377cfbe0ee7bcefd84d
Gonçalo <goncaloperalta@ua.pt>
2024-11-23 14:15:31 +0000

    adding control code

---
+++ control-code/control_signal.py
@@ -0,0 +1,9 @@
+
+def ControCode():
+    while True:
+        #sh.sem_SSH_ready.acquire()      # Wait for the a SSH connection
+        
+        # Control Code goes here...
+        break
+    
+        #sh.sem_readings_ready.release() # Signal that the Readings are ready to send
