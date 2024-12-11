80ccab0051a151eda56b4a09fde51b2e9fd65291
goncaloperalta <goncaloperalta@ua.pt>
2024-12-11 16:36:06 +0000

    cleaning and commenting the test code

--- testControlCircuit.py
+++ testControlCircuit.py
@@ -1,75 +1,81 @@
 # Pin 11 (board mode): H-Bridge (enable)
-# Pin 13 (board mode): H-Bridge (Forward)
-# Pin 15 (board mode): H-Bridge (Reverse)
-
+# Pin 13 (board mode): Current Sensor (Reset)
+# Pin 12 (board mode): H-Bridge (Forward)
+# Pin 32 (board mode): H-Bridge (Reverse)
 import RPi.GPIO as GPIO
 from time import sleep
 
 GPIO.setmode(GPIO.BOARD)
 
-# Enable
-GPIO.setup(11, GPIO.OUT)
-GPIO.output(11, GPIO.LOW)
-GPIO.setup(11, GPIO.IN)
+ENABLE = 11
+RESET = 13
+FORWARD = 12
+REVERSE = 32
 
-# Reset
-GPIO.setup(13, GPIO.OUT)
-GPIO.output(13, GPIO.HIGH) # D
+# Enable Pin
+GPIO.setup(ENABLE, GPIO.OUT)
+GPIO.output(ENABLE, GPIO.LOW) 	# Disabled 
+GPIO.setup(ENABLE, GPIO.IN) 	# Enabled (Open drain)
 
-# PWM Forward
-GPIO.setup(12, GPIO.OUT)
+# Reset Pin
+GPIO.setup(RESET, GPIO.OUT)
+#GPIO.output(RESET, GPIO.LOW)	# Activates the current sensor circuit
+GPIO.output(RESET, GPIO.HIGH)	# Deactivates the current sensor circuit
 
-# PWM Reverse
-GPIO.setup(32, GPIO.OUT)
-GPIO.output(32, GPIO.LOW)
+# PWM Forward Pin 
+GPIO.setup(FORWARD, GPIO.OUT)		# Controls the Actuator in one direction
+pFORWARD = GPIO.PWM(FORWARD, 1000)	# 1 kHz
+pFORWARD.start(0)
 
-p12 = GPIO.PWM(12, 1000)
-p12.start(0)
-p32 = GPIO.PWM(32, 1000)
-p32.start(0)
+# PWM Reverse Pin
+GPIO.setup(REVERSE, GPIO.OUT)		# Controls the Actuator in the oposite direction
+#GPIO.output(REVERSE, GPIO.LOW)
+pREVERSE = GPIO.PWM(REVERSE, 1000)	# 1 kHz
+pREVERSE.start(0)
+
 
 for i in range(5):
 	# Forward
-	p12.ChangeDutyCycle(50)
+	pFORWARD.ChangeDutyCycle(50)
 
-	#GPIO.output(13, GPIO.HIGH) # D
+	#GPIO.output(RESET, GPIO.HIGH) # D
 	#sleep(1)
-	#GPIO.output(13, GPIO.LOW) # A
+	#GPIO.output(RESET, GPIO.LOW) # A
 	#sleep(1)
-	#GPIO.output(13, GPIO.HIGH) # D
+	#GPIO.output(RESET, GPIO.HIGH) # D
 	#sleep(1)
-	#GPIO.output(13, GPIO.LOW) # A
+	#GPIO.output(RESET, GPIO.LOW) # A
 	#sleep(1)
 
-	#GPIO.setup(11, GPIO.IN)
+	#GPIO.setup(ENABLE, GPIO.IN)
 	#sleep(1)
-	#print(GPIO.input(11))
-	#GPIO.setup(11, GPIO.OUT)
+	#print(GPIO.input(ENABLE))
+	#GPIO.setup(ENABLE, GPIO.OUT)
 	#sleep(1)
-	#GPIO.setup(11, GPIO.IN)
+	#GPIO.setup(ENABLE, GPIO.IN)
 	#sleep(1)
-	#print(GPIO.input(11))
-	#GPIO.setup(11, GPIO.OUT)
+	#print(GPIO.input(ENABLE))
+	#GPIO.setup(ENABLE, GPIO.OUT)
 	#sleep(1)
-	#GPIO.setup(11, GPIO.IN)
-	#print(GPIO.input(11))
+	#GPIO.setup(ENABLE, GPIO.IN)
+	#print(GPIO.input(ENABLE))
 
 	sleep(5)
 
-	p12.ChangeDutyCycle(0)
+	pFORWARD.ChangeDutyCycle(0)
 
-	GPIO.output(13, GPIO.LOW)
+	GPIO.output(RESET, GPIO.LOW)
 
 	# Reverse
-	p32.ChangeDutyCycle(75)
+	pREVERSE.ChangeDutyCycle(75)
 
 	sleep(3)
 
-	p32.ChangeDutyCycle(0)
+	pREVERSE.ChangeDutyCycle(0)
 
-	GPIO.output(13, GPIO.HIGH)
+	GPIO.output(RESET, GPIO.HIGH)
 
-p32.stop()
-p12.stop()
-
+# Clean
+pREVERSE.stop()
+pFORWARD.stop()
 GPIO.cleanup()
