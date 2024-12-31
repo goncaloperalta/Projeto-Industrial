7d2d3a14dbb16ea57b61e253df83ccc7fde2b199
Gonçalo <goncaloperalta@ua.pt>
2024-12-31 13:50:09 +0000

    stable api reference page done

--- interface/src/routes/APIDocumentation/+page.svelte
+++ interface/src/routes/APIDocumentation/+page.svelte
@@ -1,61 +1,183 @@
 <script>
-   
+    import Endpoint from "./Endpoint.svelte";
 </script>
 
-<main class="dark:bg-slate-700 text-white h-screen text-xl">
-    <div>
-
-    </div>
-    <div class=" flex ">
-        <div class=" mt-32 mb-20 mx-10">
-            <strong class=" text-2xl">Table of contents</strong>
-        </div>
-
-        <div class="h-screen border border-black"></div>
-
-        <div class="ml-20 mt-14 w-3/4">
-            <strong class=" text-3xl ">API Reference</strong>
-
-            <div>
-                Our system provides a RESTful API on port <code class="px-1 bg-slate-800 rounded-sm">:8000</code>. <br>
-                The API accepts and returns only <code class="px-1 bg-slate-800 rounded-sm">JSON</code> encoded objects with no need for authentication. <br>
-                There are diferent ways to use the API, one is with <code class="px-1 bg-slate-800 rounded-sm">Python</code>:
-                <div class="bg-slate-800 my-3 rounded-md py-5 px-4">            
-<pre><code>import requests
+<main class="bg-slate-800 text-white text-xl h-auto">
+    <div class="flex">
+        <div class="mx-auto my-14 w-3/4">
+            <strong class="text-3xl">API Reference</strong>
+            <div class="my-4">
+                This web interface runs on port <code class="px-1 bg-slate-900 rounded-sm">:5173</code>, however the system also provides a RESTful API on port <code class="px-1 bg-slate-900 rounded-sm">:8000</code>. Anything done in this web interface is based on this API.<br>
+                The API accepts and returns <u>only</u> <code class="px-1 bg-slate-900 rounded-sm">JSON</code> encoded objects with no need for authentication. There are diferent ways to use the it, one is with <code class="px-1 bg-slate-900 rounded-sm">Python</code>:
+                <div class="bg-slate-900 my-3 rounded-md py-5 px-4">            
+                    <pre><code>import requests
 url = 'http://&lt;ip&gt;:8000/&lt;Endpoint&gt;'
 headers = &lbrace;
-    "content-type": "application/json"
+    "Content-type": "application/json"
 &rbrace;
 
 res = requests.get(url, headers=headers)
-print(res.text)
-</code></pre>
-                                    </div>
+print(res.text)     </code></pre>
+                </div>
+                In each of the endpoints bellow it's provided the expected return and a sample code to trigger it.
             </div>
-            
-            <hr>
-
-            <div class="bg-slate-500 flex h-12 rounded-sm my-3">
-                <div class=" py-2 px-7 bg-green-500 rounded-sm">GET</div>
-                <div class=" py-2 px-7"><code class="px-1 bg-gray-700 rounded-sm">/api</code></div>
-                <div class="my-auto ml-auto pr-5">&#11167</div>
-            </div>
-            <div class="px-4">
-                Returns a message if the API is up and running.
-                <div class="bg-slate-800 my-3 rounded-md py-5 px-4">            
-<pre><code>&lbrace;
+                <hr class="my-2">
+                <h2 class="italic font-bold">STATUS</h2>
+                <Endpoint verb="GET" endpoint="/api" message="Returns a message if the API is up and running."
+                            jsonReturn='&lbrace;
     "message": "API running."
-&rbrace;</code></pre>
-                </div>
-                Sample <code class="px-1 bg-slate-800 rounded-sm">python</code> code:
-                <div class="bg-slate-800 my-3 rounded-md py-5 px-4">
-<pre><code>import requests
+&rbrace;'
+                            code='import requests
 
-res = requests.get('http://&lt;ip&gt;:8000:/api')
-print(res.text)
-</code></pre>
-                </div>
-            </div>
+url = "http://localhost:8000/api"
+headers = &lbrace;
+    "Content-type": "application/json"
+&rbrace;
+res = requests.get(url, headers=headers)
+print(res.text)'/>
+                <Endpoint verb="GET" endpoint="/state" message="Returns a message with the current state which can be on of the following:"
+                            jsonReturn='&lbrace;
+    "message": "READY"              // Ready to start a test
+    "message": "RUNNING A TEST"     // Already running a test
+&rbrace;'
+                            code='import requests
+
+url = "http://localhost:8000/status"
+headers = &lbrace;
+    "Content-type": "application/json"
+&rbrace;
+res = requests.get(url, headers=headers)
+print(res.text)'/>
+
+                <h2 class="italic font-bold">RUN A TEST</h2>
+                <Endpoint verb="POST" endpoint="/start" message="Starts a button test, with the given parameters or profile chosen. If a valid profile is given the other parameters are ignored. A request with no body starts the test with all parameters set to zero. Returns one of the following messages: "
+                            jsonReturn='&lbrace;
+    "message": "Test started."              // Status code 200 OK
+    "message": "A Test is already running." // Status code 304 Not Modified
+    "message": "Profile not found"          // Status code 404 Not Found
+    "detail": [                             // Status code 422 Unprocessable Entity
+        ...
+    ]
+&rbrace;'
+                            code='import requests
+
+url = "http://localhost:8000/status"
+headers = &lbrace;
+    "Content-type": "application/json"
+&rbrace;
+res = requests.get(url, headers=headers)
+print(res.text)'/>
+
+                <h2 class="italic font-bold">TESTS DATA</h2>
+                <Endpoint verb="GET" endpoint="/get-tests" message="Returns all tests done, from oldest to newest, with format: "
+                            jsonReturn='&lbrace;
+    "tests": &lbrace;
+        "test": [
+            &lbrace;
+                "id": 5,
+                "button": "INFO",
+                "success": 1,
+                "force_val": "[[1, 2, 3, 4, 5], [1, 2, 3, 4, 5]]",
+                "time_val": "[[1, 2, 3, 4, 5], [1, 2, 3, 4, 5]]",
+                "date": "2024-12-30",
+                "time": "11:45:43"
+            &rbrace;,
+            ...
+        ]
+    &rbrace;
+&rbrace;'
+                            code='import requests
+
+url = "http://localhost:8000/get-tests"
+headers = &lbrace;
+    "Content-type": "application/json"
+&rbrace;
+res = requests.get(url, headers=headers)
+print(res.text)'/>
+                <Endpoint verb="GET" endpoint="/get-last-test" message="Returns only the last test done: "
+                            jsonReturn='&lbrace;
+    "button": 7,
+    "success": "INFO",
+    "force_val": 1,
+    "time_val": "[[1, 2, 3, 4, 5]]",
+    "date": "[[1, 2, 3, 4, 5]]",
+    "time": "2024-12-30"
+&rbrace;'
+                            code='import requests
+
+url = "http://localhost:8000/get-last-test"
+headers = &lbrace;
+    "Content-type": "application/json"
+&rbrace;
+res = requests.get(url, headers=headers)
+print(res.text)'/>
+                <Endpoint verb="GET" endpoint="/get-success" message="Returns the success of all tests done: "
+                            jsonReturn='[
+    1,
+    1,
+    0,
+    ...
+]'
+                            code='import requests
+
+url = "http://localhost:8000/get-success"
+headers = &lbrace;
+    "Content-type": "application/json"
+&rbrace;
+res = requests.get(url, headers=headers)
+print(res.text)'/>
+
+                <h2 class="italic font-bold">PROFILES</h2>
+                <Endpoint verb="GET" endpoint="/get-profiles" message="Returns all profiles: "
+                            jsonReturn='&lbrace;
+    "profiles": &lbrace;
+        "profile": [
+            &lbrace;
+                "id": 1,
+                "pName": "Custom",
+                "pressTime": 0,
+                "nTimes": 0,
+                "interval": 0
+            &rbrace;,
+            ...
+        ]
+    &rbrace;
+&rbrace;'
+                            code='import requests
+
+url = "http://localhost:8000/get-profiles"
+headers = &lbrace;
+    "Content-type": "application/json"
+&rbrace;
+res = requests.get(url, headers=headers)
+print(res.text)'/>
+                <Endpoint verb="POST" endpoint="/add-profile" message="Add a new profile with given parameters and name. Returns one of the messages: "
+                            jsonReturn='&lbrace;
+    "message": "Profile added to database."                 // Status code 201 Created
+    "message": "A profile with that name already exists."   // Status code 400 Bad Request
+&rbrace;'
+                            code='import requests
+
+url = "http://localhost:8000/add-profile"
+headers = &lbrace;
+    "Content-type": "application/json"
+&rbrace;
+res = requests.get(url, headers=headers)
+print(res.text)'/>
+
+                <Endpoint verb="DELETE" endpoint="/delete-profile" message="Deletes a profile given it's name. Returns on of the messages: "
+                            jsonReturn='&lbrace;
+    "message": "Profile deleted from database."     // Status code 200 OK
+    "message": "Profile name not found."            // Status code 404 Not Found
+&rbrace;'
+                            code='import requests
+
+url = "http://localhost:8000/delete-profile"
+headers = &lbrace;
+    "Content-type": "application/json"
+&rbrace;
+res = requests.get(url, headers=headers)
+print(res.text)'/>
         </div>
     </div>
 </main>
---
+++ interface/src/routes/APIDocumentation/Endpoint.svelte
@@ -0,0 +1,35 @@
+<script>
+    let { verb, endpoint, message, jsonReturn, code } = $props();
+    
+    let shDetails = $state(0);
+    let arrow = $state('⮟');
+    function details(){
+        shDetails = !shDetails;
+        if(arrow == '⮟'){
+            arrow = '⮝';
+        } else{
+            arrow = '⮟';
+        }
+    }
+    
+</script>
+
+<!-- svelte-ignore a11y_click_events_have_key_events -->
+<div role="button" tabindex="0" onclick={details} class="bg-slate-700 flex h-12 rounded-sm my-3">
+    <div class="w-24 rounded-sm flex {verb == "GET" ? "bg-green-500" : verb == "POST" ? "bg-blue-600" : "bg-red-600"}"><div class="m-auto">{verb}</div></div>
+    <div class="py-2 px-7"><code class="px-1 bg-gray-800 rounded-sm">{endpoint}</code></div>
+    <div class="my-auto ml-auto pr-7">{arrow}</div>
+</div>
+
+{#if shDetails}
+<div class="px-4 my-4">
+    {message}
+    <div class="bg-slate-900 my-4 rounded-md py-5 px-4">            
+        <pre><code>{jsonReturn}</code></pre>
+    </div>
+    Sample <code class="px-1 bg-slate-900 rounded-sm">python</code> code:
+    <div class="bg-slate-900 my-3 rounded-md py-5 px-4">
+        <pre><div class="whitespace-pre-warp" id="codebox">{code}</div></pre>
+    </div>
+</div>
+{/if}
