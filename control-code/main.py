e723fc79334c4f70535c8ffef0d94a99e9dbf5b9
goncaloperalta <goncaloperalta@ua.pt>
2025-01-08 18:27:43 +0000

    reverting commented code

--- control-code/main.py
+++ control-code/main.py
@@ -2,10 +2,10 @@ import uvicorn
 import logging
 import threading
 from api import app
-# from state import State
-# from ssh import SSHConnect
-# from sensor_reader import SensorReader
-# from control_signal import ControlCode
+from state import State
+from ssh import SSHConnect
+from sensor_reader import SensorReader
+from control_signal import ControlCode
 
 def setupLogging():
     logging.basicConfig(filename='app.log', level=logging.DEBUG, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
@@ -17,22 +17,22 @@ def main():
     setupLogging()
 
     APIth = threading.Thread(target=StartAPI)
-    # STATEth = threading.Thread(target=State)
-    # SSHth = threading.Thread(target=SSHConnect)
-    # CONTROLth = threading.Thread(target=ControlCode)
-    # SENSORth = threading.Thread(target=SensorReader)
+    STATEth = threading.Thread(target=State)
+    SSHth = threading.Thread(target=SSHConnect)
+    CONTROLth = threading.Thread(target=ControlCode)
+    SENSORth = threading.Thread(target=SensorReader)
     
     APIth.start()
-    # STATEth.start()
-    # SSHth.start()
-    # CONTROLth.start()
-    # SENSORth.start()
+    STATEth.start()
+    SSHth.start()
+    CONTROLth.start()
+    SENSORth.start()
 
     APIth.join()
-    # STATEth.join()
-    # SSHth.join()
-    # CONTROLth.join()
-    # SENSORth.join()
+    STATEth.join()
+    SSHth.join()
+    CONTROLth.join()
+    SENSORth.join()
 
 if __name__ == '__main__':
     main()
