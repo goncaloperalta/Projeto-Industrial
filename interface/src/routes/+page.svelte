955215ecec19f8ff2fcce6388660a4bcb8bd2b48
Gonçalo <goncaloperalta@ua.pt>
2025-01-18 22:20:44 +0000

    removing uselless line

--- interface/src/routes/+page.svelte
+++ interface/src/routes/+page.svelte
@@ -4,7 +4,6 @@
 
     let url = "http://localhost:8000/"
     if(typeof document != "undefined"){
-        hostname = window.location.hostname
         url = `http://${window.location.hostname}:8000/`
     }
     
--- interface/src/routes/history/+page.svelte
+++ interface/src/routes/history/+page.svelte
@@ -3,7 +3,6 @@
 	
 	let url = "http://localhost:8000/"
     if(typeof document != "undefined"){
-        hostname = window.location.hostname
         url = `http://${window.location.hostname}:8000/`
     }
 
